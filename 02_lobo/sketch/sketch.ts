75606b5f3345c319560d147ff61b2ce6ce3b8104
unknown <silvaberros@gmail.com>
2021-10-08 09:06:20 -0300

    atualizando o lobo

---
+++ 02_lobo/sketch/sketch.ts
@@ -0,0 +1,125 @@
+
+class personagem {
+  //ATRIBUTOS
+  x: number;
+  y: number;
+  passo: number;
+  imagem: p5.Image;
+
+  //PARAMETROS
+  constructor(x: number, y: number, passo: number, imagem: p5.Image) {
+    this.x = x;
+    this.y = y;
+    this.passo = passo;
+    this.imagem = imagem;
+  }
+
+  //METODOS
+  draw() {
+    image(this.imagem, this.x * this.passo, this.y * this.passo, this.passo, this.passo);
+  }
+}
+
+class tabuleiro {
+  nl: number;
+  nc: number;
+  passo: number;
+  background: p5.Image;
+
+  constructor(nc: number, nl: number, passo: number, background: p5.Image) {
+    this.nl = nl;
+    this.nc = nc;
+    this.passo = passo;
+    this.background = background;
+  }
+
+  draw() {
+    image(this.background, 0, 0, this.nc * this.passo, this.nl * this.passo);
+    for(let x = 0; x < this.nc; x++) {
+      for(let y = 0; y < this.nl; y++) {
+        noFill();
+        stroke(200, 200, 200);
+        strokeWeight(2);
+        rect(x * this.passo, y * this.passo, this.passo, this.passo);
+      }
+    }
+  }
+}
+
+let wolf_image_D: p5.Image;
+let wolf_image_E: p5.Image;
+let wolf: personagem;
+let rabbit_image: p5.Image;
+let rabbit: personagem;
+let board_image: p5.Image;
+let board: tabuleiro;
+let buraco: personagem;
+let buraco_image: p5.Image;
+
+function loadImg (path: string):p5.Image {
+  return loadImage(
+    path,
+    () => console.log("Loading" + path + "ok"),
+    () => console.log("Loading" + path + "error")
+  );
+}
+
+function preload() {
+  board_image = loadImg('../sketch/grama.jpg');
+  wolf_image_E = loadImg('../sketch/lobol.png');
+  wolf_image_D = loadImg('../sketch/lobol2.png');
+  rabbit_image = loadImg('../sketch/coelho.png');
+  buraco_image = loadImg('../sketch/buraco.png');
+}
+
+function wolf_limit() {
+  if(wolf.x == board.nc){
+    wolf.x--;
+  }
+  if(wolf.x < 0){
+    wolf.x++;
+  }
+  if(wolf.y == board.nl){
+    wolf.y--;
+  }
+  if(wolf.y < 0){
+    wolf.y++;
+  }
+}
+
+////////////////////////////////////////////
+function setup() {
+  let size = 150;
+  wolf = new personagem(2, 2, size, wolf_image_D);
+  rabbit = new personagem(floor(random(1, 5)), floor(random(1, 5)), size, rabbit_image);
+  board = new tabuleiro(5, 5, size, board_image);
+  buraco = new personagem(floor(random(1, 5)), floor(random(1, 5)), size, buraco_image);
+  createCanvas(board.nc * size, board.nl * size);
+
+}
+
+function draw() {
+  board.draw();
+  if( wolf.x === buraco.x && wolf.y === buraco.y) {
+    buraco.draw();
+  }
+  wolf.draw();
+  if( wolf.x === rabbit.x && wolf.y === rabbit.y) {
+    rabbit.draw();
+  }
+  wolf_limit(); 
+}
+
+function keyPressed () {
+  if(keyCode === LEFT_ARROW) {
+    wolf.x--;
+    wolf.imagem = wolf_image_E;
+  } else if(keyCode === RIGHT_ARROW) {
+    wolf.x++;
+    wolf.imagem = wolf_image_D;
+  } else if(keyCode === UP_ARROW) {
+    wolf.y--;
+  } else if(keyCode === DOWN_ARROW) {
+    wolf.y++;
+  }  
+}
